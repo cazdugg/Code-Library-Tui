2bb63afe5ea80d4d88cb9c2a2575a94f5bf1ca93
cazdugg <carolinesynnott@aol.com>
2015-10-20 17:24:35 +0100

    Create header.php

---
+++ thomson/master/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns:v="urn:schemas-microsoft-com:vml">
+
+
+<br/>
+<head>
+<meta name="viewport" content="width=full-width, initial-scale=1.0" />
+<title>Thomson - Holidays Designed for You.</title>
+<style type="text/css">
+/* General styles and reset */
+<?php include '/home/codio/workspace/thomson/master/css_styles.php' ?>
