4578a2ab5bbe855b0df0071e22373eacc30e6ce9
cazdugg <carolinesynnott@aol.com>
2015-10-20 17:13:27 +0100

    bannersthomson.php

---
+++ bannersthomson.php
@@ -0,0 +1,69 @@
+<?php include 'includes/header.php' ?>
+
+
+
+<div class="container">
+    <div class="intro">
+        <h1>Thomson Main Banner Templates</h1>
+    </div>       
+</div>
+<?php    
+$xml=simplexml_load_file("thomson/MainBanners.xml") or die("Error: Cannot create object");
+foreach($xml->children() as $B) { ?>
+
+  
+<section class="banner thomson">
+    <div class="container">
+        
+     <h1><?php echo $B->title ?></h1>
+     
+        <div class="row">
+             <div class="col-md-8">
+             <div class="pad">
+                 <iframe scrolling="no" src="thomson/mainBanners/<?php echo $B->html ?>" width="750" height="<?php echo $B->height ?>"></iframe>    
+             </div> 
+            </div>
+            <div class="col-md-4">
+               
+                
+                 <div class="pad">
+                    <iframe src="thomson/mainBanners/<?php echo $B->html ?>" width="340" height="<?php echo $B->height ?>"></iframe>
+                </div>
+            </div>
+           
+        </div>
+        <div class="row">
+            <div class= "col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Code to copy for Main Banner <button class="html" id="<?php echo $B->id ?>" data-clipboard-text="Select">Select all</button></div>
+                    <div class="panel-body">
+                        <textarea id="textArea<?php echo $B->id ?>">
+                            <?php echo $B->code ?>
+                        </textarea>
+                    </div>
+                </div>
+            
+            </div>
+                        <div class= "col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">CSS to copy for this particular banner <button class="css" id="<?php echo $B->id ?>" data-clipboard-text="Select">Select all</button> </div>
+                    <div class="panel-body">
+                        <textarea id="css_<?php echo $B->id ?>">
+                            <?php echo $B->css ?>
+                        </textarea>
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+          
+</div>
+            </section>
+<?php
+
+} 
+?>
+
+<?php include 'includes/footer.php' ?>
+
+
