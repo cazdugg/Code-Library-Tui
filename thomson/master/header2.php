edff4d90f98f3676e37539108e2dfa509e84ca8b
cazdugg <carolinesynnott@aol.com>
2015-10-20 17:25:02 +0100

    Create header2.php

---
+++ thomson/master/header2.php
@@ -0,0 +1,34 @@
+}
+/* classes for auto complete */
+
+.device-width, .full-width, .fluidimage, .nomargin, .nopad, .mobile-hide, .mobile-show, .mobile-drop, .mobile-float, .height-reset, .no-border, .mainbox-width, .280-width, .EMPtile, .EMPtile-image, .EMPtile-map, .EMPtile-image-wide, .EMPtile-tb, .EMPtile-textheight, .bg-img-swap, .bg-img-shift, .EMPtile-padding, .pad10, .padbtm, .margintop10 {
+	animation: none;
+}
+</style>
+</head>
+<body bgcolor="#FFFFFF" text="#333333" style="background:#FFFFFF; min-height:1000px; color:#333333; -webkit-font-smoothing: antialiased;">
+<table width="100%" cellspacing="0" border="0" cellpadding="0">
+  <!-- Super subject line -->
+  <tr>
+    <td valign="top" style="font-family:Arial, Helvetica, sans-serif; color:#ffffff; font-size:1px;">This line is visible in MS Office desktop notifications</td>
+  </tr>
+  <!-- end of super subject line -->
+  <tr>
+    <td style="padding:5px 0;" align="center" valign="top"><!--wrapper open-->
+      
+      <table bgcolor="#f0f0f0" class="device-width" id="yahoo-resize" width="750" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td align="center" valign="top"><!--container open--> 
+            <!-- email webpage link -->
+            
+            <table bgcolor="#ffffff" class="full-width" align="center" width="750" border="0" cellspacing="0" cellpadding="0">
+              <tbody>
+                <tr>
+                  <td style="font-family: Helvetica, Arial, sans-serif; font-size:10px; line-height:12px; color:#6c6c6c;" align="center"> Email not looking right?&nbsp; <a style="color:#6c6c6c; text-decoration:underline;" href="%%track {[return http://email.thomson.co.uk/preview/rss.tml?$tracking]} -group {rss}%%">Click here</a> to view it in your browser</td>
+                </tr>
+                <tr>
+                  <td style="font-size:0px; line-height:0px;"><img src="http://cslb.creatormail.co.uk/sites/tui/thomson/TH131213/spacer.gif" style="display:block"  width="15" height="10" border="0" /></td>
+                </tr>
+              </tbody>
+            </table>
+            <!-- end of email webpage link --> 
