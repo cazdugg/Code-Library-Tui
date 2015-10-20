cac6d0d1850081229de2d9a177834e490992f4d7
cazdugg <carolinesynnott@aol.com>
2015-10-20 17:23:36 +0100

    Create css_styles.php

---
+++ thomson/master/css_styles.php
@@ -0,0 +1,526 @@
+body {
+	width: 100% !important;
+	cursor: pointer;
+}
+.ReadMsgBody {
+	width: 100%;
+}
+/* Force Hotmail to display emails at full width and center content */
+.ExternalClass {
+	width: 100%;
+}
+.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {
+	line-height: 100%;
+}
+body {
+	-webkit-text-size-adjust: none;
+	-ms-text-size-adjust: none;
+	-webkit-font-smoothing: antialiased;
+} /* Prevent Webkit and Windows Mobile platforms from changing default font sizes plus webkit font weight correction. */
+body {
+	margin: 0;
+	padding: 0;
+	min-height: 1000px;
+}
+img {
+	line-height: 100%;
+	outline: none;
+	text-decoration: none;
+	display: block;
+}
+/* Prevent spacing issues in outlook */
+table, table td {
+	border-collapse: collapse !important;
+}
+.float-right {
+	float: right;
+	display: inline-block;
+}
+.float-left {
+	float: left;
+	display: inline-block;
+}
+#yahoo-resize[yahoofix] {
+	width: 620px;
+}
+
+/* Media queries begin here */
+@media screen {
+/* droid regular */
+@font-face {
+	font-family: 'droid-sans-reg';
+	src: url('http://cslb.creatormail.co.uk/sites/tui/fonts/DroidSans-webfont.eot');
+	src: url('http://cslb.creatormail.co.uk/sites/tui/fonts/DroidSans-webfont.eot?#iefix') format('embedded-opentype'), url('http://cslb.creatormail.co.uk/sites/tui/fonts/DroidSans-webfont.woff') format('woff'), url('http://cslb.creatormail.co.uk/sites/tui/fonts/DroidSans-webfont.svg#droid_sansregular') format('svg'), url('http://cslb.creatormail.co.uk/sites/tui/fonts/DroidSans-webfont.ttf') format('truetype');
+	font-weight: normal;
+	font-style: normal;
+}
+/* droid bold */  
+@font-face {
+	font-family: 'droid-sans-bold';
+	src: url('http://cslb.creatormail.co.uk/sites/tui/fonts/DroidSans-Bold-webfont.eot');
+	src: url('http://cslb.creatormail.co.uk/sites/tui/fonts/DroidSans-Bold-webfont.eot?#iefix') format('embedded-opentype'), url('http://cslb.creatormail.co.uk/sites/tui/fonts/DroidSans-Bold-webfont.woff') format('woff'), url('http://cslb.creatormail.co.uk/sites/tui/fonts/DroidSans-Bold-webfont.svg#droid_sansbold') format('svg'), url('http://cslb.creatormail.co.uk/sites/tui/fonts/DroidSans-Bold-webfont.ttf') format('truetype');
+	font-weight: normal;
+	font-style: normal;
+}
+/* tui type */
+@font-face {
+	font-family: 'tui';
+	src: url('http://cslb.creatormail.co.uk/sites/tui/fonts/tuitype-webfont.eot');
+	src: url('http://cslb.creatormail.co.uk/sites/tui/fonts/tuitype-webfont.eot?#iefix') format('embedded-opentype'), url('http://cslb.creatormail.co.uk/sites/tui/fonts/tuitype-webfont.woff') format('woff'), url('http://cslb.creatormail.co.uk/sites/tui/fonts/tuitype-webfont.svg#webfont') format('svg'), url('http://cslb.creatormail.co.uk/sites/tui/fonts/tuitype-webfont.ttf') format('truetype');
+	font-weight: normal;
+	font-style: normal;
+}
+}
+/* FONT IMPORTS */
+
+body {
+	background-color: #ffffff !important;
+}
+/**/ .invisible {
+	max-height: 0px !important;
+	font-size: 0 !important;
+	display: none !important;
+	line-height: 0px !important;
+	padding-top: 0px !important;
+	padding-bottom: 0px !important;
+	padding-right: 0px !important;
+	padding-left: 0px !important;
+	mso-hide: all !important;
+}
+
+@media only screen and (max-width: 480px) {
+/* Prevent iOS from blue linking random elements. */
+a[href^="tel"], a[href^="sms"] {
+	text-decoration: none;
+	color: white;
+	pointer-events: none;
+	cursor: default;
+}
+.mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
+	text-decoration: default;
+	color: white !important;
+	pointer-events: auto;
+	cursor: default;
+}
+/* Global text adjustments */
+.fontsizeXXXL[class="fontsizeXXXL"] {
+	font-size: 42px !important;
+	line-height: 120% !important;
+}
+.fontsizeXXL[class="fontsizeXXL"] {
+	font-size: 32px !important;
+	line-height: 130% !important;
+}
+.fontsizeXL[class="fontsizeXL"] {
+	font-size: 28px !important;
+	line-height: 130% !important;
+}
+.fontsizeL[class="fontsizeL"] {
+	font-size: 24px !important;
+	line-height: 130% !important;
+}
+.fontsizeM[class="fontsizeM"] {
+	font-size: 20px !important;
+	line-height: 130% !important;
+}
+.fontsizeA[class="fontsizeA"] {
+	font-size: 18px !important;
+	line-height: 130% !important;
+}
+.fontsizeB[class="fontsizeB"] {
+	font-size: 16px !important;
+	line-height: 130% !important;
+}
+.fontsizeC[class="fontsizeC"] {
+	font-size: 13px !important;
+	line-height: 130% !important;
+}
+.fontsizeD[class="fontsizeD"] {
+	font-size: 12px !important;
+	line-height: 130% !important;
+}
+.fontsizeE[class="fontsizeE"] {
+	font-size: 10px !important;
+	line-height: 130% !important;
+}
+
+/* offer table list vlasses */
+*[class*="offer-list"] {
+	display: block !important;
+	width: 100% !important;
+	padding-bottom: 15px;
+}
+*[class*="DOD-details"] {
+	width: 100% !important;
+	padding-top: 25px !important;
+	border-top: 1px solid #808080 !important;
+}
+*[class*="offer-list-Imgbox"] {
+	display: block !important;
+	width: 100% !important;
+	height: 180px !important;
+	overflow: hidden !important;
+	vertical-align: middle !important;
+}
+*[class*="offer-list-DODImgbox"] {
+	height: 215px !important;
+	display: block !important;
+	width: 100% !important;
+	overflow: hidden !important;
+	vertical-align: middle !important;
+}
+*[class*="mob-trating"] {
+	padding-top: 10px;
+	width: 100px !important;
+	height: 15px !important;
+}
+.accordion {
+	width: 100%;
+	height: auto !important;
+	margin-bottom: 10px;
+	cursor: pointer;
+	display: block;
+	position: relative;
+}
+div.button {
+	display: block !important;
+	height: 17px !important;
+	width: 115px;
+}
+/**/ .accordion div.content {
+	background: #edebe5;
+	height: 0;
+	overflow: hidden;
+	transition: height 0.3s ease;
+	cursor: pointer;
+}
+/**/ .accordion:hover div.content {
+	cursor: pointer;
+	height: 140px;
+}
+/**/ .accordion:active div.content {
+	cursor: pointer;
+	height: 140px;
+}
+*[class*="resultIcon"] {
+	vertical-align: top !important;
+	padding-top: 0.3em !important;
+	padding-right: 5px !important;
+	width: 22px !important;
+	height: 19px !important;
+}
+*[class*="resultText"] {
+	font-size: 15px !important;
+	vertical-align: middle !important;
+	line-height: 220% !important;
+	padding-bottom: 5px !important;
+}
+/* END offer table list classes */
+
+/* main css for thomson mailings */
+/**/ .invisible {
+	max-height: none !important;
+	font-size: 12px !important;
+	display: block !important;
+	height: 17px !important;
+	width: 115px !important;
+	overflow : visible !important;
+}
+*[class*="device-width"] {
+	width: 320px!important;
+}
+*[class*="full-width"] {
+	height: auto !important;
+	width: 100% !important;
+}
+*[class*="mainbox-width"] {
+	width: 95% !important;
+	margin-left: auto !important;
+	margin-right: auto !important;
+}
+
+*[class=resize90] {
+	height: auto !important;
+	width: 90% !important;
+}
+*[class*="280-width"] {
+	width: 280px !important;
+}
+
+*[class*="fluidimage"] {
+	height: auto !important;
+	width: 100% !important;
+}
+*[class*="mobile-hide"], *[class*="mobile-hide"] * {
+	display: none !important;
+	max-height: 0 !important;
+	max-width: 0 !important;
+	height: 0 !important;
+	width: 0px !important;
+	padding-top: 0px !important;
+	padding-bottom: 0px !important;
+	padding-right: 0px !important;
+	padding-left: 0px !important;
+	margin-top: 0px !important;
+	margin-right: 0px !important;
+	margin-bottom: 0px !important;
+	margin-left: 0px !important;
+	visibility: hidden !important;
+	line-height: 0px !important;
+	font-size: 0px !important;
+	background: none !important;
+}
+*[class*="mobile-show"], *[class*="mobile-show"] * {
+	display: inline-block !important;
+	visibility: visible !important;
+	height: auto !important;
+	width: auto !important;
+	max-height: none !important;
+	min-height: none !important;
+	max-width: none !important;
+	min-width: none !important;
+}
+*[class*="mobile-drop"] {
+	float: none !important;
+	clear: both !important;
+	width: 100% !important;
+	padding-top: 0px !important;
+	padding-bottom: 0px !important;
+	padding-right: 0px !important;
+	padding-left: 0px !important;
+	display: block !important;
+}
+td[class*="mobile-drop"] {
+	display: table-cell !important;
+}
+table[class*="mobile-drop"] {
+	display: table !important;
+}
+tr[class*="mobile-drop"] {
+	display: table-row !important;
+}
+*[class*="mobile-float"] {
+	display: block !important;
+	width: 100% !important;
+}
+*[class*="nomargin"] {
+	margin-top: 0px !important;
+	margin-right: 0px !important;
+	margin-bottom: 0px !important;
+	margin-left: 0px !important;
+}
+.margintop10[class="margintop10"] {
+	margin-top: 10px !important;
+}
+*[class*="nopad"] {
+	padding-top: 0px !important;
+	padding-bottom: 0px !important;
+	padding-right: 0px !important;
+	padding-left: 0px !important;
+}
+.pad10[class="pad10"] {
+	padding-top: 10px !important;
+	padding-right: 10px !important;
+	padding-bottom: 10px !important;
+	padding-left: 10px !important;
+}
+.padbtm[class="padbtm"] {
+	padding-bottom: 15px !important;
+}
+*[class="height-reset"], *[id="height-reset"] {
+	height: auto !important;
+}
+*[class*="no-border"] {
+	border: none !important;
+}
+*[class*="teaser-box"] {
+	display: block !important;
+	width: 100% !important;
+	height: auto !important;
+	margin-bottom: 35px !important;
+}
+td[class="footer-box-border"] {
+	border: none!important;
+	padding: 0!important;
+	width: 290px!important;
+}
+table[class="footer-box"] {
+	width: 100%!important;
+}
+
+/* main banner classes */
+*[class*="bg-img-swap"] {
+	background-position: right !important;
+	background: url(http://cslb.creatormail.co.uk/sites/tui/PDP/sys/th/mobImg.jpg) top left !important;
+	background-repeat: no-repeat !important;
+	background-size: cover !important;
+	width: 100% !important;
+	height: auto !important;
+	background-position: right !important;
+}
+*[class*="bg-img-shift"] {
+	background-repeat: no-repeat !important;
+	background-position: 0px 108px !important;
+	width: 100% !important;
+	height: auto !important;
+}
+*[class*="header-cell"] {
+	background: url(http://cslb.creatormail.co.uk/sites/tui/thomson/TH150713/header-mob.gif) no-repeat top left !important;
+}
+*[class*="header-spacer"] {
+	height: 68px !important;
+}
+/* end of main banner classes */
+
+/* EMP classes */
+*[class*="EMPtile"], *[class*="EMPtile-image"], *[class*="EMPtile-map"] {
+	width: 100% !important;
+	height: 320px !important;
+	border: 0px !important;
+	vertical-align: bottom !important;
+}
+*[class*="EMPtile-image-wide"] {
+	width: 100% !important;
+	height: auto !important;
+	border: 0px !important;
+	vertical-align: bottom !important;
+}
+*[class*="EMPtile-tb"] {
+	width: 100% !important;
+	height: 320px !important;
+	border: 0px !important;
+	background-color: #74afdc !important;
+	background-size: 235% !important;
+}
+*[class*="EMPtile-btn"] {
+	width: auto !important;
+	height: 30px !important;
+	padding-top: 20px !important;
+	vertical-align: top !important;
+}
+*[class*="EMPtile-textheight"] {
+	height: 35px !important;
+}
+*[class*="EMPtile-padding"] {
+	width: 100% !important;
+	height: 330px !important;
+	border: 0px !important;
+}
+/* end EMP classes */
+
+/* odd rules that may get deleted if not used at all */
+/*
+
+*[class*="linebreak"], *[id*="linebreak"] {
+	display: block !important;
+	white-space: pre-line;
+}
+.marginbtm10[class="marginbtm10"] {
+	margin-bottom: 10px !important;
+}
+.padtop[class="padtop"] {
+	padding-top: 10px !important;
+}
+.padtop20[class="padtop20"] {
+	padding-top: 20px !important;
+}
+padright[class="padright"] {
+	padding-right: 5px !important;
+}
+
+padleft[class="padleft"] {
+	padding-left: 5px !important;
+}
+*[class*="mobile-bg-color"] {
+	background-color: #F8F0E5 !important;
+}
+*[class*="mobile-float-bot-bord"] {
+	display: block !important;
+	width: 100% !important;
+	border-bottom-style: solid;
+	border-bottom-width: 1px;
+	border-bottom-color: #abd0ea;
+}
+*[class*="mobile-img-resize"] {
+	width: 100% !important;
+	height: 161px !important;
+}
+*[class*="mobile-resize"] {
+	width: 100% !important;
+	height: 161px !important;
+	background-size: cover !important;
+}
+
+*[class*="prp-col1"] {
+	display: block !important;
+	width: 100% !important;
+	height: auto !important;
+	padding-bottom: 20px !important;
+}
+*[class*="prp-col2"] {
+	width: 100% !important;
+	height: 90px !important;
+}
+
+
+/* Align in mobile with inline-block unsure how used this is, I think not at all */
+/*
+*[class*="mobile-center"], *[id*="mobile-center"] {
+	text-align: center !important;
+}
+img[class*="mobile-align"] {
+	display: inline-block !important;
+}
+table[class*="mobile-align"] {
+	display: inline-block !important;
+	width: auto !important;
+}
+*[class*="mobile-left"] {
+	text-align: left !important;
+}
+*[class*="mobile-right"] {
+	text-align: right !important;
+}
+*[class*="center-block"] {
+	margin-left: auto !important;
+	margin-right: auto !important;
+	text-align: center !important;
+}
+
+/*more rando classes */
+/*
+*[class*="upgrades-img-shift"] {
+	background-position: -260px bottom !important;
+	background-size: auto !important;
+	background-color: #f8f1e7 !important;
+	height: 325px !important;
+}
+td[class="map-mobile"] {
+	width: 100%!important;
+	background: url(http://cslb.creatormail.co.uk/sites/tui/thomson/click_fup/map/mobmap.jpg) no-repeat top left!important;
+	background-size: cover !important;
+	height: 433px !important;
+}
+a[class="map-mobile"] {
+	display: block !important;
+	height: 433px;
+	text-decoration: none;
+	border-style: none;
+}
+[class=resize70] {
+	height: auto !important;
+	width: 70% !important;
+	padding-bottom: 15px !important;
+}
+
+[class=mobile-float-box-bottom] {
+	display: block !important;
+	width: 100% !important;
+	padding-bottom: 5px;
+}
+[class=mobile-float-box-top] {
+	display: block !important;
+	width: 100% !important;
+	padding-top: 5px;
+}
+*/
