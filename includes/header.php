39311f8ad4c063377d18358b7087118581a07165
cazdugg <carolinesynnott@aol.com>
2015-10-20 17:30:08 +0100

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<title>Code Library</title>
+<!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <link href="js/jquery.bxslider.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>    
+    <script src="js/jquery.bxslider.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/main.js"></script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+<nav class="navbar">
+  <div class="container">
+    <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+         
+        </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav  navbar-nav">
+          <li class="dropdown"><a href="index.php">Home</a></li>
+          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Thomson</a>
+          <ul class="dropdown-menu">
+            <li><a href="masterThomson.php">Master Template</a></li>
+            <li><a href="bannersthomson.php">Main banners</a></li>
+            <li><a href="subBanners_thomson.php">Sub banners</a></li>
+            <li><a href="emps_thomson.php">EMP tiles</a></li>
+            <li><a href="#">Other</a></li>
+          </ul>
+        </li>
+        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">First Choice</a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Blank template</a></li>
+            <li><a href="bannersfirstchoice.php">Main banners</a></li>
+            <li><a href="subBanners_firstChoice.php">Sub banners</a></li>
+            <li><a href="#">EMP tiles</a></li>
+            <li><a href="#">Other</a></li>
+          </ul>
+        </li>
+        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Thomson Airways</a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Blank template</a></li>
+            <li><a href="#">Main banners</a></li>
+            <li><a href="#">Sub banners</a></li>
+            <li><a href="#">EMP tiles</a></li>
+            <li><a href="#">Other</a></li>
+          </ul>
+        </li>
+        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Thomson Cruises</a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Blank template</a></li>
+            <li><a href="#">Main banners</a></li>
+            <li><a href="#">Sub banners</a></li>
+            <li><a href="#">EMP tiles</a></li>
+            <li><a href="#">Other</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <!--/.nav-collapse --> 
+  </div>
+</nav>
