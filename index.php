71010815f9af0b4f9add1038336369c0246b7c1d
cazdugg <carolinesynnott@aol.com>
2015-10-20 17:14:45 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include 'includes/header.php' ?>
+<div class="container">
+  <div class="intro">
+    <h1>Welcome to the TUI eCrm team's code library. This should be used for briefing in and building email campaigns. </h1>
+    <p class="lead"> You can use the menu to find sections of the email you are building and, working with a blank template, add them together to have a feature-complete html file that only needs updated assets. </p>
+  </div>
+</div>
+<div class="container-fluid trendbg">
+    <div class="container">
+        <section class="instructions">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h2>1</h2>
+                    <p>Download the master template for your email.</p>
+                </div>
+                <div class="col-sm-3">
+                    <h2>2</h2>
+                    <p>Choose a main banner &amp; copy code into template where instructed. </p>
+                </div>
+                <div class="col-sm-3">
+                    <h2>4</h2>
+                    <p>Copy specific css for chosen banner into css of master template.</p>
+                </div>
+                <div class="col-sm-3">
+                    <h2>5</h2>
+                    <p>Choose a sub banner, EMP section and copy code and css into template.</p>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+<?php include 'includes/footer.php' ?>
