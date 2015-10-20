9ab675c09732071795b351def809c0bfde2b0433
cazdugg <carolinesynnott@aol.com>
2015-10-20 17:16:06 +0100

    Create masterThomson.php

---
+++ masterThomson.php
@@ -0,0 +1,42 @@
+<?php include 'includes/header.php' ?>
+<div class="container">
+    <div class="intro">
+        <h1>Master Template</h1>
+        <p>This is your starting point...</p>
+         <p><a href="/thomson/master-test-working.html.zip" target="_blank">Thomson Master Template</a></p>
+           <p>To begin an email campaign please download our master template here:</p>
+            <p>This template includes the header and the footer of the email only. There are three commented sections within the master template as shown below. </p>
+    </div> 
+    <div class="row">
+        <div class="col-sm-8">
+         
+            <div class="well">
+                <textarea style="border:none; height:110px; width:100%; overflow:hidden; resize:none; font-size:14px; text-align:left; padding:0; margin:0;">
+    <!-- Main Banner-->
+    Choose a main banner and paste it in here
+    <!--Main Banner End-->
+                </textarea>  
+            </div>
+              <div class="well">
+                <textarea style="border:none; height:110px; width:100%; overflow:hidden; resize:none; font-size:14px; text-align:left; padding:0; margin:0;">
+    <!-- Sub Banner-->
+    Choose a sub banner and paste it in here
+    <!--Sub Banner End-->
+                </textarea>  
+            </div>
+              <div class="well">
+                <textarea style="border:none; height:110px; width:100%; overflow:hidden; resize:none; font-size:14px; text-align:left; padding:0; margin:0;">
+    <!-- EMP Section -->
+    Copy your EMP section into here
+    <!--EMP Section End-->
+                </textarea>  
+            </div>
+            
+            
+            
+           
+            
+        </div>
+    </div>
+</div>
+<?php include 'includes/footer.php' ?>
